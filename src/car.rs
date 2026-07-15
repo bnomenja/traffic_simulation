7f34898e616abaed120ca122c68b10d2fc8af3e8
unknown <kyler6612@gmail.com>
2026-07-15 01:23:28 +0100

    test friendly version

--- src/car.rs
+++ src/car.rs
@@ -11,25 +11,24 @@ pub enum Direction {
     West, 
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Car {
     pub x: f32,
     pub y: f32,
     pub direction: Direction,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-impl Direction {
+//impl Direction {
     //generate a random direction
-    pub fn random() -> Self {
-    }
-}
+//    pub fn random() -> Self {
+//    }
+//}
 
 
 impl Car {
     //generate a new car based on the direction of the input 
-    pub fn new(direction: Direction) -> Self {
-    }
+    //pub fn new(direction: Direction) -> Self {
+    //}
 
     // change the position of the car
     pub fn update(&mut self, dt: f32, is_green: bool, ahead_progress: Option<f32>) {
@@ -47,15 +46,15 @@ pub struct CarManager {
 }
 
 impl CarManager {
-    pub fn new() -> Self {
-    }
+    //pub fn new() -> Self {
+    //}
 
     // try to spawn a car if it is allowed
     pub fn try_spawn_car(&mut self, direction: Direction) {
     }
 
     // update the position of each cars, if it is out of the simuation, remove it.
-    pub fn update(&mut self, t: f32, , lights: &TrafficController) {
+    pub fn update(&mut self, t: f32, lights: &TrafficController) {
     }
 
     // draw all the cars
